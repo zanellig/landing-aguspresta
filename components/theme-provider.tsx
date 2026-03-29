91f80095d0387c61bfc83535c6ca837f46236eac
zanellig <gonzalozanelli1@gmail.com>
2026-03-29 00:10:52 -0300

    fix(theme): change default theme to dark and disable system theme toggle
    Updated the ThemeProvider component to set the default theme to dark and disabled the system theme toggle for a more consistent user experience.

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -10,8 +10,8 @@ function ThemeProvider({
 	return (
 		<NextThemesProvider
 			attribute="class"
-			defaultTheme="system"
-			enableSystem
+			defaultTheme="dark"
+			enableSystem={false}
 			disableTransitionOnChange
 			{...props}
 		>
